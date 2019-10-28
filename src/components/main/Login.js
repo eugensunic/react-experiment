703d230ffc06156aceea294b5309f739cf3bda01
eugensunic <eugen.sunic@gmail.com>
2019-10-28 17:42:24 +0100

    refactor login using hooks only

--- src/components/main/Login.js
+++ src/components/main/Login.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import { mockLoginRequest } from "../../helpers/mock-call";
 
 import {
   isUsernameValid,
@@ -7,14 +8,46 @@ import {
   isPasswordLessThan5
 } from "../../services/login.service.js";
 
-function Login() {
+function Login(props) {
   const [obj, setCredential] = useState({
     username: "",
     password: "",
     passwordError: null,
     usernameError: null,
-    submitRequest: false
+    submitRequest: false,
+    loginSuccess: false
   });
+
+  useEffect(() => {
+    if (!obj.loginSuccess) return;
+    console.log("success");
+    localStorage.setItem("user", "bam");
+    props.history.push("/profile");
+  }, [obj.loginSuccess]);
+
+  useEffect(() => {
+    if (!obj.submitRequest) return;
+    setTimeout(
+      _ =>
+        mockLoginRequest("/database/users.json")
+          .then(res =>
+            validCredentials(res)
+              ? setCredential({
+                  ...obj,
+                  passwordError: "",
+                  loginSuccess: true,
+                  submitRequest: false
+                })
+              : setCredential({
+                  ...obj,
+                  passwordError: "Wrong credentials, Try again",
+                  submitRequest: false
+                })
+          )
+          .catch(x => console.log("error occurred", x)),
+      1000
+    );
+  }, [obj.submitRequest]);
 
   const isFrontendValid = (username, password) => {
     return isUsernameValid(username) && !isPasswordLessThan5(password);
@@ -43,7 +76,6 @@ function Login() {
       } else if (isPasswordLessThan5(password)) {
         passwordErr = "Password too short";
       }
-      // one time update
       setCredential({
         ...obj,
         usernameError: usernameErr,
@@ -51,6 +83,11 @@ function Login() {
       });
       return;
     }
+    // one time update
+    setCredential({
+      ...obj,
+      submitRequest: true
+    });
   };
   return (
     <div className="row">
@@ -121,145 +158,3 @@ function Login() {
 }
 
 export default withRouter(Login);
-
-// class Login extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: "",
-//       password: "",
-//       passwordError: null,
-//       usernameError: null,
-//       submitRequest: false
-//     };
-//   }
-
-//   isFrontendValid(username, password) {
-//     return isUsernameValid(username) && !isPasswordLessThan5(password);
-//   }
-
-//   validCredentials(res) {
-//     return res.users.some(
-//       x =>
-//         this.state.username === x.username && this.state.password === x.password
-//     );
-//   }
-
-//   validateUser(username, password) {
-//     // FE validation
-//     this.setState({ passwordError: "" });
-//     if (!this.isFrontendValid(username, password)) {
-//       if (isEmpty(username)) {
-//         this.setState({ usernameError: "Please provide username" });
-//       } else if (!isUsernameValid(username)) {
-//         this.setState({ usernameError: "Please provide valid username/mail" });
-//       }
-//       if (isEmpty(password)) {
-//         this.setState({ passwordError: "Please provide password" });
-//       } else if (isPasswordLessThan5(password)) {
-//         this.setState({ passwordError: "Password too short" });
-//       }
-//       return;
-//     }
-
-//     // BE validation
-//     this.setState({ submitRequest: true });
-//     setTimeout(
-//       _ =>
-//         mockLoginRequest("/database/users.json")
-//           .then(res =>
-//             this.validCredentials(res)
-//               ? this.setState(
-//                   {
-//                     passwordError: "",
-//                     submitRequest: false
-//                   },
-//                   () => {
-//                     localStorage.setItem("user", "bam");
-//                     this.props.history.push("/profile");
-//                     this.props.loginSuccess();
-//                   }
-//                 )
-//               : this.setState({
-//                   passwordError: "Wrong crendentials, Try again",
-//                   submitRequest: false
-//                 })
-//           )
-//           .catch(x => console.log("error occurred", x)),
-//       1000
-//     );
-//   }
-
-//   render() {
-//     return (
-//       <div className="row">
-//         <div
-//           className="card card-signin my-5 center"
-//           style={{ opacity: localStorage.getItem("user") ? 0.4 : 1 }}
-//         >
-//           <div className="card-body">
-//             <h5 className="card-title text-center">Admin area</h5>
-//             <div className="form-signin">
-//               <div className="form-label-group">
-//                 <input
-//                   type="email"
-//                   id="username"
-//                   className="form-control"
-//                   placeholder="Username/mail"
-//                   required
-//                   onChange={e =>
-//                     this.setState({
-//                       username: e.target.value,
-//                       usernameError: null
-//                     })
-//                   }
-//                 />
-//                 <label htmlFor="inputEmail">Email address</label>
-//               </div>
-
-//               {this.state.usernameError && (
-//                 <div className="error-container">
-//                   {this.state.usernameError}
-//                 </div>
-//               )}
-
-//               <div className="form-label-group">
-//                 <input
-//                   type="password"
-//                   id="password"
-//                   className="form-control"
-//                   placeholder="Password"
-//                   onChange={e =>
-//                     this.setState({
-//                       password: e.target.value,
-//                       passwordError: null
-//                     })
-//                   }
-//                   required
-//                 />
-//                 <label htmlFor="inputPassword">Password</label>
-//               </div>
-//               {this.state.passwordError && (
-//                 <div className="error-container">
-//                   {this.state.passwordError}
-//                 </div>
-//               )}
-//               {this.state.submitRequest && <div className="loader"></div>}
-//               <button
-//                 className="btn btn-lg btn-primary btn-block text-uppercase"
-//                 disabled={!!localStorage.getItem("user")}
-//                 type="submit"
-//                 onClick={() =>
-//                   this.validateUser(this.state.username, this.state.password)
-//                 }
-//               >
-//                 Log in
-//               </button>
-//               <hr className="my-4" />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
